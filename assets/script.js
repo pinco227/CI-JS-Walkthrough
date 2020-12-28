5ac5ae2dd6240ec6f477e0b2261bbed76cab6330
pinco227 <p4ul.gg@gmail.com>
2020-12-28 00:08:05 +0000

    feat(js): create display Addition question, calculate answer and check answer functions
    wired up with event listeners and runGame function

--- assets/script.js
+++ assets/script.js
@@ -7,25 +7,66 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if(this.getAttribute('data-type') == 'submit') {
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame('addition');
 });
 
-function runGame() {
+function runGame(gameType) {
 
+    // Generate 2 random numbers between 1 and 25
+    // Math floor rounds down the whole number
+    // Math random generates a random number between 0 and 1
+
+    let num1 = Math.floor(Math.random() * 25)+1;
+    let num2 = Math.floor(Math.random() * 25)+1;
+
+    if(gameType == 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`;
+    }
+
+    document.getElementById('answer-box').value = "";
 }
 
 function checkAnswer() {
 
+    // Check the answer against the first element in
+    // the returned calculatedCorrectAnswer array
+
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if(isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+    
+    runGame(calculatedAnswer[1]);
 }
 
 function calculateCorrectAnswer() {
+    // Gets the operands and the operator from the DOM
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if(operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}, aborting!`;
+    }
 }
 
 function incrementScore() {
@@ -36,8 +77,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
